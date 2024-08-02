a8d8b215780df9fd802b55e7fea864f14c30ae65
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-08-02 22:19:28 +0100

    - getStaticPath and getStaticParams not really necessary since its the New app router
    - `product/[slug].js` replaced with convention:
    `product/[slug]/page.tsx`

--- app/product/[slug]/Product.tsx
+++ app/product/[slug]/Product.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Product } from "@/components";
 import {
 	AiOutlineMinus,
 	AiOutlinePlus,
@@ -17,47 +16,46 @@ const ProductView = ({ product, products }: any) => {
 	// 	setShowCart(true);
 	// };
 	return (
-		<div>
-			<div className="product-detail-container">
-				<div>
-					<div className="image-container">
-						<img
-							src={urlFor(image && image[index]).url()}
-							className="product-detail-image"
-						/>
-					</div>
-					<div className="small-images-container">
-						{image?.map((item: string, i: number) => (
-							<img
-								key={i}
-								src={urlFor(item).url()}
-								className={
-									i === index ? "small-image selected-image" : "small-image"
-								}
-								onMouseEnter={() => setIndex(i)}
-							/>
-						))}
-					</div>
+		<div className="product-detail-container">
+			<div>
+				<div className="image-container">
+					<img
+						src={urlFor(image && image[index]).url()}
+						className="product-detail-image"
+					/>
 				</div>
+				<div className="small-images-container">
+					{image?.map((item: string, i: number) => (
+						<img
+							key={i}
+							src={urlFor(item).url()}
+							className={
+								i === index ? "small-image selected-image" : "small-image"
+							}
+							onMouseEnter={() => setIndex(i)}
+						/>
+					))}
+				</div>
+			</div>
 
-				<div className="product-detail-desc">
-					<h1>{name}</h1>
-					<div className="reviews">
-						<div>
-							<AiFillStar />
-							<AiFillStar />
-							<AiFillStar />
-							<AiFillStar />
-							<AiOutlineStar />
-						</div>
-						<p>(20)</p>
+			<div className="product-detail-desc">
+				<h1>{name}</h1>
+				<div className="reviews">
+					<div>
+						<AiFillStar />
+						<AiFillStar />
+						<AiFillStar />
+						<AiFillStar />
+						<AiOutlineStar />
 					</div>
-					<h4>Details: </h4>
-					<p>{details}</p>
-					<p className="price">${price}</p>
-					<div className="quantity">
-						<h3>Quantity:</h3>
-						{/* <p className="quantity-desc">
+					<p>(20)</p>
+				</div>
+				<h4>Details: </h4>
+				<p>{details}</p>
+				<p className="price">${price}</p>
+				<div className="quantity">
+					<h3>Quantity:</h3>
+					{/* <p className="quantity-desc">
 							<span className="minus" onClick={decQty}>
 								<AiOutlineMinus />
 							</span>
@@ -66,34 +64,22 @@ const ProductView = ({ product, products }: any) => {
 								<AiOutlinePlus />
 							</span>
 						</p> */}
-					</div>
-					<div className="buttons">
-						<button
-							type="button"
-							className="add-to-cart"
-							// onClick={() => onAdd(product, qty)}
-						>
-							Add to Cart
-						</button>
-						<button
-							type="button"
-							className="buy-now"
-							// onClick={handleBuyNow}
-						>
-							Buy Now
-						</button>
-					</div>
 				</div>
-			</div>
-
-			<div className="maylike-products-wrapper">
-				<h2>You may also like</h2>
-				<div className="marquee">
-					<div className="maylike-products-container track">
-						{products.map((item: any) => (
-							<Product key={item._id} product={item} />
-						))}
-					</div>
+				<div className="buttons">
+					<button
+						type="button"
+						className="add-to-cart"
+						// onClick={() => onAdd(product, qty)}
+					>
+						Add to Cart
+					</button>
+					<button
+						type="button"
+						className="buy-now"
+						// onClick={handleBuyNow}
+					>
+						Buy Now
+					</button>
 				</div>
 			</div>
 		</div>
--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { Product } from "@/components";
 import ProductView from "./Product";
 import { client } from "@/sanity/lib/client";
 
@@ -14,10 +15,21 @@ const page = async ({ params }: Props) => {
 	const product = await client.fetch(query);
 	const products = await client.fetch(productsQuery);
 
-	console.log(product);
-	console.log(products);
-
-	return <ProductView product={product} products={products} />;
+	return (
+		<div>
+			<ProductView product={product} products={products} />;
+			<div className="maylike-products-wrapper">
+				<h2>You may also like</h2>
+				<div className="marquee">
+					<div className="maylike-products-container track">
+						{products.map((item: any) => (
+							<Product key={item._id} product={item} />
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default page;
